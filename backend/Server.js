294999ebddaeb19284c766bc8b9ac8dd0aedab1c
Mustakim Mugal <mustakimmugal824@gmail.com>
2025-08-13 00:36:59 -0700

    server.js

--- backend/Server.js
+++ backend/Server.js
@@ -52,5 +52,5 @@ app.post('/api/verify/otp', (req, res) => {
 });
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
